981ba3dd1c17f2dc5ad4661275d7e568523ead60
leminhthe04 <the.leminh04@hcmut.edu.vn>
2025-07-07 12:45:28 +0700

    Add DisconnectHandler.java, idk why it was untracked in the last commit

---
+++ src/main/java/com/server/game/netty/pipelineComponent/DisconnectHandler.java
@@ -0,0 +1,27 @@
+package com.server.game.netty.pipelineComponent;
+
+
+import com.server.game.netty.UserChannelRegistry;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DisconnectHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+
+        UserChannelRegistry.unregister(channel);
+        super.channelInactive(ctx);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println(">>> Server Exception: " + cause.getMessage());
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
