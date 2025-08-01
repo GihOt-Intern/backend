b4530a154aee2458c0dd84d24b0e6810aaa364a8
leminhthe04 <the.leminh04@hcmut.edu.vn>
2025-08-01 05:06:38 +0000

    Fix: no handler for PingReceive (caused by file auto rename :((()

--- src/main/java/com/server/game/netty/sender/NotificationSender.java
+++ src/main/java/com/server/game/netty/sender/NotificationSender.java
@@ -3,7 +3,7 @@ package com.server.game.netty.sender;
 import org.springframework.stereotype.Component;
 
 import com.server.game.annotation.customAnnotation.MessageMapping;
-import com.server.game.netty.receiveObject.attack.AttackReceive;
+import com.server.game.netty.receiveObject.PingReceive;
 import com.server.game.netty.sendObject.PongSend;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class NotificationSender {
 
-    @MessageMapping(AttackReceive.class)
-    public PongSend sendPongMessage(AttackReceive receiveObject) {
+    @MessageMapping(PingReceive.class)
+    public PongSend sendPongMessage(PingReceive receiveObject) {
         return new PongSend();
     }
 
